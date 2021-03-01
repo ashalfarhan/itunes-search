6d7419343d687e341375bb03387798fec196e560
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-03-01 20:39:10 +0700

    add some styling

--- pages/result/_id.vue
+++ pages/result/_id.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="w-11/12 mx-auto mt-4">
-    <nuxt-link to="/" class="font-poppins uppercase hover:underline"
+    <nuxt-link to="/" class="font-poppins text-white uppercase hover:underline"
       >Go back home</nuxt-link
     >
-    <h1 class="text-2xl font-poppins">
+    <h1 class="text-2xl text-white font-poppins">
       Here's the results of {{ searchResult }}
     </h1>
     <div
       v-if="results.length"
-      class="grid grid-cols-3 md:grid-cols-4 gap-8 mt-12"
+      class="grid grid-cols-1 md:grid-cols-4 gap-8 mt-12"
     >
       <app-card
         v-for="(result, idx) in results"
