583733eeabed8dade9122ae4eb87aaf93986047b
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-03-01 18:58:10 +0700

    Change result search to another page

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,43 @@
 <template>
-  <div class="container">
-    <div class=" w-11/12 mx-auto">
+  <div class="w-screen h-screen">
+    <div class="w-11/12 mx-auto flex items-center justify-center h-full">
       <form
-        class="flex justify-center items-center flex-col"
+        class="w-full flex justify-center max-w-sm mx-auto items-stretch flex-col"
         @submit.prevent="handleSubmit"
       >
         <input
           type="text"
-          placeholder="Beatles, twentyonepilots"
+          placeholder="artist, albums, single, song"
           v-model="userInput"
-          class="focus:outline-none focus:border-b-4 border-red-500"
+          class="focus:outline-none py-2 border-b-2 text-2xl border-green-600 text-center"
         />
-        <input type="submit" value="Search" />
+        <input
+          class="py-4 px-2 cursor-pointer bg-green-500 text-white font-semibold rounded-xl mt-4"
+          type="submit"
+          value="Search"
+        />
       </form>
-      <div v-if="results.length > 0 && userInput">
-        <ul>
-          <li v-for="(result, idx) in results" :key="idx">
-            <h1>{{ result.artistName }}</h1>
-            <img :src="result.artworkUrl100" />
-            <audio controls>
-              <source :src="result.previewUrl" />
-              Your browser doesn't support the audio element
-            </audio>
-          </li>
-        </ul>
-      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { API_URL } from "~/helper/constants";
 import formatSearch from "~/helper/formatSearch";
+
 interface Data {
   userInput: string;
-  results: string | string[];
 }
 
 export default Vue.extend({
   data(): Data {
     return {
-      userInput: "",
-      results: []
+      userInput: ""
     };
   },
   methods: {
-    async handleSubmit(): Promise<void> {
-      const response = await fetch(`${API_URL}${formatSearch(this.userInput)}`);
-      const data = await response.json();
-      console.log(data.results);
-      this.results = data.results;
+    handleSubmit() {
+      this.$router.push(`result/${formatSearch(this.userInput)}`);
     }
   }
 });
--- pages/result/_id.vue
+++ pages/result/_id.vue
@@ -1,4 +1,39 @@
+<template>
+  <div class="w-11/12 mx-auto mt-4">
+    <nuxt-link to="/" class="font-poppins uppercase hover:underline"
+      >Go back home</nuxt-link
+    >
+    <h1 class="text-2xl font-poppins">
+      Here's the results of {{ searchResult }}
+    </h1>
+    <div
+      v-if="results.length"
+      class="grid grid-cols-3 md:grid-cols-4 gap-8 mt-12"
+    >
+      <app-card
+        v-for="(result, idx) in results"
+        :key="idx"
+        :result="result"
+      ></app-card>
+    </div>
+  </div>
+</template>
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+import AppCard from "~/components/AppCard.vue";
+
+export default Vue.extend({
+  components: {
+    AppCard
+  },
+  middleware: "search",
+  computed: {
+    results() {
+      return this.$store.state.results;
+    },
+    searchResult() {
+      return this.$route.params.id.split("+").join(" ");
+    }
+  }
+});
 </script>
