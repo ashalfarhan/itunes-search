eb7b2b53f216ba745c4a3ebd5067c7ec1e778f04
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-03-01 20:37:58 +0700

    create method for shortening title and artist

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -2,20 +2,15 @@
   <li
     @mouseenter="handleEnter"
     @mouseleave="handleLeave"
-    class="list-none space-x-2 rounded-xl p-4 flex items-center transform bg-gradient-to-r transition ease-in-out duration-75 origin-bottom cursor-pointer hover:shadow-2xl hover:scale-125"
+    class="list-none space-x-2 bg-inherit relative card rounded-xl p-4 flex items-center transform transition ease-in-out duration-75 origin-bottom cursor-pointer hover:shadow-2xl hover:scale-110 md:hover:scale-125"
   >
     <img :src="result.artworkUrl100" />
     <div class="font-poppins">
-      <h1 class="font-medium tracking-wider">{{ result.artistName }}</h1>
+      <h1 class="font-medium tracking-wider">
+        {{ shortText(result.artistName) }}
+      </h1>
       <h1>
-        {{
-          result.trackName
-            ? String(result.trackName)
-                .split(" ")
-                .slice(0, 4)
-                .join(" ")
-            : ""
-        }}
+        {{ shortText(result.trackName) }}
       </h1>
       <p class="text-sm">
         Price: {{ result.collectionPrice }} {{ result.currency }}
@@ -39,7 +34,34 @@ export default Vue.extend({
     },
     handleLeave(e: any) {
       e.target.lastChild.pause();
+    },
+    shortText(arg: string) {
+      if (arg) {
+        return arg.split(" ").length > 4
+          ? arg
+              .split(" ")
+              .slice(0, 4)
+              .join(" ")
+              .concat("...")
+          : arg;
+      } else {
+        return "";
+      }
     }
   }
 });
 </script>
+<style scoped>
+.card::after:hover {
+  content: "";
+  border-radius: 10px;
+  background: inherit;
+  position: absolute;
+  left: -10px;
+  right: 0;
+  top: 0px;
+  bottom: 0;
+  box-shadow: inset 0 0 0 200px rgba(255, 255, 255, 0.05);
+  filter: blur(1px);
+}
+</style>
