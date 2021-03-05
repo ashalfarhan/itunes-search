53ce30e5ce060533cb869acda7851212b5cbcac9
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-03-06 02:11:19 +0700

    Use const helper

--- middleware/search.ts
+++ middleware/search.ts
@@ -1,11 +1,10 @@
 import { Context } from "@nuxt/types";
 import axios from "axios";
+import { API_URL } from "~/helper/constants";
 
 export default async function({ params, store }: Context) {
   try {
-    const response = await axios.get(
-      `https://itunes.apple.com/search?term=${params.id}`
-    );
+    const response = await axios.get(`${API_URL}${params.id}`);
     const {
       data: { results }
     } = response;
